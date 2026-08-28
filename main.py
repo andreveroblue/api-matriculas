fe1838148c82c8ee550ac0e3a2346a7521c10e94
andreveroblue <andreveroblue@gmail.com>
2026-08-28 18:04:00 -0500

    Update validation rules for matricula request

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI(
+    title="API Sistema de Matrículas",
+    description="API educativa para realizar pruebas de caja negra",
+    version="1.0"
+)
+
+
 class SolicitudMatricula(BaseModel):
     cedula: str | None = None
     nivel: int | None = None
@@ -6,53 +16,56 @@ class SolicitudMatricula(BaseModel):
     numero_asignaturas: int
 
 
+@app.get("/")
+def inicio():
+    return {
+        "sistema": "Sistema de Matrículas",
+        "estado": "API funcionando"
+    }
+
+
 @app.post("/api/matriculas/validar")
 def validar_matricula(datos: SolicitudMatricula):
 
-    # RN06 - Cédula obligatoria
-    if datos.cedula is None:
+    # RN06 - Cédula y nivel son obligatorios
+    if datos.cedula is None or datos.nivel is None:
         raise HTTPException(
             status_code=400,
-            detail="La cédula es obligatoria "
+            detail="Los campos cédula y nivel son obligatorios"
         )
 
-    # RN07 - Nivel obligatorio
-    if datos.nivel is None:
-        raise HTTPException(
-            status_code=400,
-            detail="El nivel es obligatorio"
-        )
-
-    # RN01
+    # RN01 - Validación del nivel
     if datos.nivel < 1 or datos.nivel > 10:
         raise HTTPException(
             status_code=400,
             detail="El nivel académico debe estar entre 1 y 10"
         )
 
-    # RN02
+    # RN02 - Impedimento académico
     if datos.impedimento_academico:
         raise HTTPException(
             status_code=400,
             detail="El estudiante posee impedimento académico"
         )
 
-    # RN03
+    # RN03 - Impedimento financiero
     if datos.impedimento_financiero:
         raise HTTPException(
             status_code=400,
             detail="El estudiante posee impedimento financiero"
         )
 
-    # RN04
+    # RN04 - Número de asignaturas
     if datos.numero_asignaturas < 1 or datos.numero_asignaturas > 6:
         raise HTTPException(
             status_code=400,
             detail="El número de asignaturas debe estar entre 1 y 6"
         )
 
+    # Matrícula aprobada
     return {
         "cedula": datos.cedula,
+        "nivel": datos.nivel,
         "estado": "APROBADA",
         "puede_matricularse": True,
         "orden_pago": "GENERADA",
